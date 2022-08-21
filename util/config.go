7b844fe6dc28580aaf7d97022fd079df7444b3de
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-08-21 16:20:17 +0700

    Modify env file

--- util/config.go
+++ util/config.go
@@ -13,8 +13,7 @@ type Config struct {
 // LoadConfig reads configuration from file or environment variables.
 func LoadConfig(path string) (config Config, err error) {
 	viper.AddConfigPath(path)
-	viper.SetConfigName("app")
-	viper.SetConfigType("env")
+	viper.SetConfigFile(".env")
 
 	viper.AutomaticEnv()
 
